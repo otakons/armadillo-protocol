a823fcfc2e273f1fbc356cde4aa5280c311f7d30
RamiN821 <rami.niskanen@gmail.com>
2025-11-10 22:11:26 +0200

    feat player scene

---
+++ game/player_character.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+##Speed in pixels per second
+@export_range(0, 1000) var speed := 60
+
+func _physics_process(_delta: float) -> void:
+	get_player_input()
+	move_and_slide()
+	
+func get_player_input() -> void:
+	var vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = vector * speed
