d0532b3c4813d4be250c49e8cba73aebed824079
Erik Karsten <erik@opcode.xyz>
2025-11-18 19:45:53 +0200

    feat: build linux binary with nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,29 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.stdenv.mkDerivation {
+            pname = "armadillo-protocol";
+            version = "0.1.0";
+            src = ./game;
+            buildInputs = with pkgs.godotPackages; [ godot export-template ];
+
+            buildPhase = ''
+              export HOME=$TMPDIR
+
+              mkdir -p work/build
+              cp -r $src/* work/
+              mkdir -p $HOME/.local/share/godot
+
+              ln -s ${pkgs.godotPackages.export-template}/share/godot/export_templates $HOME/.local/share/godot/
+              ls $HOME/.local/share/godot/export_templates
+
+              godot --headless --export-release "Linux" --path work
+
+              mkdir -p $out
+              cp -r work/build/* $out/
+            '';
+
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             godot_4_3
